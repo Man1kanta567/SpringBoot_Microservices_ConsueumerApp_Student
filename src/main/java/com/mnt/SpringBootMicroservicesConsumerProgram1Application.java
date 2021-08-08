5709de861abbb3580ec2593c222a9467fc6b0216
TL <manik@Manikanta>
2021-08-08 12:03:03 +0530

    Microservices consumerApp basic config

---
+++ src/main/java/com/mnt/SpringBootMicroservicesConsumerProgram1Application.java
@@ -0,0 +1,13 @@
+package com.mnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesConsumerProgram1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesConsumerProgram1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnt/SpringBootMicroservicesConsumerProgram1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesConsumerProgram1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
